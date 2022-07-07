158114fba614db89c8c8410b669478560117c138
m.oraee <m.oraee1376@gmail.com>
2022-07-07 11:46:18 +0430

    add single post page + clean up

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,19 +1,41 @@
-import {getPostById, getPosts} from '../../lib/posts';
-import React from "react";
+import {getPostById, getPosts} from '../../lib/posts'
+import React from "react"
 import Head from 'next/head'
 import Link from 'next/link'
+import Paper from "@mui/material/Paper"
+import Typography from "@mui/material/Typography"
+import {Button} from "@mui/material"
+import Box from "@mui/material/Box"
 
 export default function Post(props) {
+
+  const post = props.post.data
 
   return (
   <>
     <Head>
-      <title>{props.data.data.title}</title>
-      <meta content={props.data.data.body} name="description"></meta>
+      <title>{post.title}</title>
+      <meta content={post.body} name="description"></meta>
     </Head>
-    <div>
-      <Link href="/">back</Link>
-    </div>
+      <Box m={30} mt={10}>
+        <Paper>
+          <Box p={4}>
+          <Typography align='center' variant="h5" component="h3">
+            {post.title}
+          </Typography>
+          </Box>
+          <Typography align='center' component="p">
+            {post.body}
+          </Typography>
+          <Typography align='center' component="p">
+            id : {post.id}
+          </Typography>
+          <Typography align='center' component="p">
+            user id : {post.userId}
+          </Typography>
+          <Button><Link href="/">back</Link></Button>
+        </Paper>
+      </Box>
     </>
   )
 }
@@ -30,10 +52,10 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({params}) {
-  const data = await getPostById(params)
+  const post = await getPostById(params)
   return{
     props: {
-      data
+      post
     }
   }
 }
